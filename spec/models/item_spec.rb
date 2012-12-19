65117ab9f3e8b27817b248678dc91f232f9d2b00
Daniel <d@friis.me>
2012-12-19 13:12:45 +0100

    Minor test change

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,11 @@ describe Item do
 
   it { should respond_to(:title) }
   it { should respond_to(:list_id) }
+
+  it { should be_valid }
+
+  describe "when list_id not present" do
+  	before { @item.list_id = nil }
+  	it { should_not be_valid }
+  end
 end
