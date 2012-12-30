c26361edd96efa49948f8a19d939673303a381d7
Daniel <d@friis.me>
2012-12-30 18:00:48 +0100

    Remove http-access-protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-	if Rails.env == "production"
-		http_basic_authenticate_with :name => "frodo", :password => "thering" 
-	end
   protect_from_forgery
   include SessionsHelper
 
