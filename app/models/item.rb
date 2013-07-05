7a27b9ba60e098b44d4f4215e6c15a10d075fb50
Daniel Friis <d@friis.me>
2013-07-05 20:42:09 +0200

    New popular sql query second try

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,7 +30,7 @@ class Item < ActiveRecord::Base
     end_date = Time.now
   	joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
         .select("count(distinct(case when ('impressions.created_at' BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as counter, impressionable_id, items.title, items.id, items.image")
-        .group('items.id')
+        .group('items.id', 'impressions.impressionable_id')
         .order("counter desc")
   end
 
