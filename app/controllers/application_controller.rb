ffae201bd1e8ae4b703b11f2398494347f59fad7
Daniel Friis <d@friis.me>
2013-08-05 12:08:52 +0200

    Fix bookmarklet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-  before_filter :hostname
-
-  def hostname
-  	@hostname = request.host || "www.mydomain.com"
-  end
-
-
 end
