d37b4c2989b7de025a70b7504666a44f956823ec
Daniel Friis <d@friis.me>
2013-11-23 20:25:25 +0100

    Fix popluar feed

--- app/models/item.rb
+++ app/models/item.rb
@@ -59,7 +59,7 @@ class Item < ActiveRecord::Base
       where(Item.arel_table[:via].not_eq("no_link"))
           .joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
           .select("items.*, count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as counter, impressionable_id")
-          .group('items.id')
+          .group('items.id', 'impressions.impressionable_id')
           .where("items.id IN (#{not_hidden})", false: false)
           .order("counter desc")
     end
