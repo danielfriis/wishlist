019b541385efad5cfce28a8b52daf80f23416973
Daniel Friis <d@friis.me>
2013-11-24 15:41:14 +0100

    Add case insentivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -95,7 +95,7 @@ class User < ActiveRecord::Base
 
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("name like ?", "%#{query}%") 
+    where("upper(name) like upper(?)", "%#{query}%") 
   end
 
   def self.most_followers
