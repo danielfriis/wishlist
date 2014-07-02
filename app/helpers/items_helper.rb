853f45fe08cd1736510c751fe4dd332458a946f7
Daniel Friis <d@friis.me>
2014-07-02 19:57:25 +0200

    Fix newrelic missing http accept language headers

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,12 +1,14 @@
 module ItemsHelper
 
 	def exchanged_price(item)
-    if request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'da'
-      new_price = item.price.exchange_to("DKK")
-      "#{new_price.currency.iso_code}" + " #{new_price.format(:symbol => false)}"
-    else
-      "#{item.price.currency.iso_code}" + " #{item.price.format(:symbol => false)}"
-    end
+    if request.env['HTTP_ACCEPT_LANGUAGE']
+	    if request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'da' 
+	      new_price = item.price.exchange_to("DKK")
+	      "#{new_price.currency.iso_code}" + " #{new_price.format(:symbol => false)}"
+	    else
+	      "#{item.price.currency.iso_code}" + " #{item.price.format(:symbol => false)}"
+	    end
+	  end
   end
 
 end
