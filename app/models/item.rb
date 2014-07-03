e56c4267b327684dbb76076d977b5262d2ec3349
Daniel Friis <d@friis.me>
2014-07-03 16:20:59 +0200

    Improve popular alogorithm

--- app/models/item.rb
+++ app/models/item.rb
@@ -67,14 +67,12 @@ class Item < ActiveRecord::Base
       end_date = Time.now
       not_hidden = "SELECT item_id FROM wishes WHERE hide = :false"
       with_pictures
-          .joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
-          .select("items.*, count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as view_count, impressionable_id")
-          .group('items.id', 'impressions.impressionable_id')
           .joins("left join wishes on item_id = items.id")
-          .select("items.*, count(wishes.id) as wish_count")
-          .group("items.id")
+          .joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
+          .select("items.*, (count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) * count(wishes.id)) as counter, impressionable_id")
+          .group('items.id', 'impressions.impressionable_id')
           .where("items.id IN (#{not_hidden})", false: false)
-          .order("(view_count * wish_count) desc, items.created_at desc")
+          .order("counter desc, items.created_at desc")
     end
   end
 
