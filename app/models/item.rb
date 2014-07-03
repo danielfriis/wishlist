d560744e108a4429df5ac1093f84120e50ba440d
Daniel Friis <d@friis.me>
2014-07-03 14:31:32 +0200

    Improve popular alogorithm

--- app/models/item.rb
+++ app/models/item.rb
@@ -69,10 +69,10 @@ class Item < ActiveRecord::Base
       with_pictures
           .joins("left join wishes on item_id = items.id")
           .joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
-          .select("items.*, count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as counter, impressionable_id")
+          .select("items.*, count(wish.id) as wish_count, count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as counter, impressionable_id")
           .group('items.id', 'impressions.impressionable_id')
           .where("items.id IN (#{not_hidden})", false: false)
-          .order("counter desc, items.created_at desc")
+          .order("counter * wish_count desc, items.created_at desc")
     end
   end
 
