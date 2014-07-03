cdd8064cdbef3e2f13ee861ee3d9e240a52e9f23
Daniel Friis <d@friis.me>
2014-07-03 14:27:57 +0200

    Improve popular alogorithm

--- app/models/item.rb
+++ app/models/item.rb
@@ -67,6 +67,7 @@ class Item < ActiveRecord::Base
       end_date = Time.now
       not_hidden = "SELECT item_id FROM wishes WHERE hide = :false"
       with_pictures
+          .joins("left join wishes on item_id = items.id")
           .joins("left join impressions on impressions.impressionable_id = items.id and impressions.impressionable_type = 'Item'")
           .select("items.*, count(distinct(case when (impressions.created_at BETWEEN '#{start_date}' AND '#{end_date}') then ip_address end)) as counter, impressionable_id")
           .group('items.id', 'impressions.impressionable_id')
