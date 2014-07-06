d64616e0eb7ccbfb0c59879082491713294f4dbe
Daniel Friis <d@friis.me>
2014-07-06 12:43:41 +0200

    Add reservation feature

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,12 @@
 class ReservationsController < ApplicationController
   before_filter :signed_in_user
+  before_filter :correct_user, only: [:create, :destroy]
   include Analyzable
 
 	def create
 		@wish = Wish.find(params[:reservation][:wish_id])
 		current_user.reservations.create!(wish_id: @wish.id)
+		tracker.track(current_user.id, "Reserved a gift")
 		respond_to do |f|
 			f.html { redirect_to @wish.list }
 			f.js
@@ -14,10 +16,17 @@ class ReservationsController < ApplicationController
 	def destroy
 		@wish = Reservation.find(params[:id]).wish
 		current_user.reservations.find(params[:id]).destroy
+		tracker.track(current_user.id, "Unreserved a gift")
 		respond_to do |format|
-      format.html { redirect_to @wish }
+      format.html { redirect_to @wish.list }
       format.js
     end
 	end
 
+	private
+
+    def correct_user
+      @user = Wish.find(params[:reservation][:wish_id]).list.user
+      redirect_to(root_path) unless current_user.following?(@user)
+    end
 end
