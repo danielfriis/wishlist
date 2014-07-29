84884b2b2d5bd46ceeb2007040221d70fa2d8d55
Daniel Friis <d@friis.me>
2014-07-29 16:15:28 +0200

    Finishing typeahead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     if @user.save
       UserMailer.delay.signup_confirmation(@user.id) # When using delayed_job for actionmailer '.deliver' is omitted
       @user.delay.subscribe_email
-      @user.lists.create!(name: "General")
+      @user.lists.create!(name: "My Wish List")
       sign_in @user
       flash[:success] = "Thanks for signing up!"
       tracker.alias(@user.id, cookies[:mp_distinct_id]) if cookies[:mp_distinct_id]
