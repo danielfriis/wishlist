8cffc798ad997c29e47925b837b7d761265c6cd9
Daniel Friis <d@friis.me>
2014-09-17 14:36:31 +0200

    Add no activity view

--- app/assets/javascripts/application/activities.js
+++ app/assets/javascripts/application/activities.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
 		var e = $(this);
 		e.unbind("click");
 		var see_more = "<div class='activity text-center'><a href='/activities'>See more</a></div>"
+		var nothing = "<div class='activity text-center'>No new activity.<br/> Try following more people <a href='/users'>here</a></div>"
 		$.ajax({
 		  url: e.data('url'),
 		  success: function(d) {
-		    e.popover({ content: d + see_more }).popover("show");
+		  	if (d) {
+		  		e.popover({ content: d + see_more }).popover("show");
+		  	} else {
+	  			e.popover({ content: nothing }).popover("show");
+	  		}
 		  }
 		});
 		return false
