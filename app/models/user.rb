5f5bb9f12a891ae4f53db67a88b0de88b5b48e70
Daniel Friis <d@friis.me>
2014-09-29 16:44:46 +0200

    Fix serialize array on pg

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
 
-  serialize :fb_friends
+  serialize(:fb_friends, Array)
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
