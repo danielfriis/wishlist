84ffb2d025c1c51ba3a87dfbb2f3857e7aed3cc2
Daniel Friis <d@friis.me>
2014-10-16 10:37:55 +0200

    New wish list overview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @lists = @user.lists.allowed(current_user)
-    @wishes = @user.wishes.rank(:row_order)
     @list = current_user.lists.build if signed_in?
   end
 
